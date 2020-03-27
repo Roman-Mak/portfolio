b4144c51ccffa2313ff9cae61a7a8306047c2459
Roman-Mak <meller1758@gmail.com>
2020-03-27 15:11:52 +0300

    Updates

---
+++ precache-manifest.fe4ec355d9d5087e2f284cf42ac07bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9971d7bfe83f75f3dd97abd0961c423c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "690123134cb229ed7a24",
+    "url": "/portfolio/static/css/main.179c6711.chunk.css"
+  },
+  {
+    "revision": "69c4a785a4f72924a918",
+    "url": "/portfolio/static/js/2.07a08363.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.07a08363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690123134cb229ed7a24",
+    "url": "/portfolio/static/js/main.4a9ca92c.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
